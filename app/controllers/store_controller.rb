47ee3821aa596529aeceac3a2a9fa2dbe58cd138
Harrison Lopes <harrisonlopeslb@gmail.com>
2023-05-09 21:59:29 -0300

    Adding Cart and LineItem

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -1,5 +1,10 @@
 class StoreController < ApplicationController
+  include StoreIndexCounter
+  
+  before_action :set_counter, only: [:index]
+
   def index
+    @access_counter = session[:counter]
     @products = Product.order(:title)
   end
 end
